8ec90dd8d041a0a7245e71c23269648fa63094eb
moezthemaster <moez.selmi1986@gmail.com>
2022-03-16 23:42:40 +0100

    Create committer.groovy

---
+++ committer.groovy
@@ -0,0 +1,17 @@
+                stage('Bump version and push') {
+                    withCredentials([usernamePassword(credentialsId: 'genericCreds', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USER')]) {
+                        sh "git fetch --prune origin 'refs/tags/*:refs/tags/*' '+refs/heads/*:refs/remotes/origin/*'"
+                        sh "su automation -c \"bumpversion ${release_type} --allow-dirty\""
+                        
+                        // push version update and clean pipeline var
+                        sh """sed -i 's/^run_pipeline[" "]*=[" "]*true/run_pipeline=false/I' jenkins/job.properties"""
+
+                        sh "git add VERSION setup.cfg jenkins/job.properties setup.py"
+                        sh 'git commit -m "Version "`cat VERSION`'
+                        sh "git push 'https://${GIT_USER}:${GIT_PASSWORD}@${gitRepo}' HEAD:master"
+
+                        // tag creation
+                        if (!release_type.startsWith('dev')) {
+                            sh "su automation -c \"bumpversion release --tag --tag-name=`cat VERSION` --allow-dirty\""
+                            sh "git push 'https://${GIT_USER}:${GIT_PASSWORD}@${gitRepo}' `cat VERSION`"
+                        }
