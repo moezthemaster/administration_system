798aaa2a299c15ce659bb16c230af2323b334e6c
moezthemaster <moez.selmi1986@gmail.com>
2026-01-22 15:31:53 +0100

    Update Cmd.sh

--- Cmd.sh
+++ Cmd.sh
@@ -1,1 +1,3 @@
 awk '/^---$/ && !in_comment {p=0} /DEV1/ && !p && !in_comment {p=1} p && !in_comment {print} /^[[:space:]]*#/ {next} {sub(/[[:space:]]*#.*$/, "")} !/^[[:space:]]*$/ && !/^[[:space:]]*#/ {in_comment=0} /^[[:space:]]*#/ {in_comment=1}' fichier.yaml
+
+https://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publishers/ms-vscode-remote/vsextensions/remote-ssh/0.121.2025072915/vspackage
