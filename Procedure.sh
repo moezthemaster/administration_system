7316ac6401b3e9d050195069a8d9f98a95d84633
moezthemaster <moez.selmi1986@gmail.com>
2026-04-07 14:16:33 +0100

    Update Procedure.sh

--- Procedure.sh
+++ Procedure.sh
@@ -14,3 +14,11 @@ tar -xzf ~/vscode-server-latest.tar.gz
 # Notez le nom du dossier créé
 mv vscode-server-* ${VOTRE_COMMIT_ID}
 touch ${VOTRE_COMMIT_ID}/0
+
+{
+    "remote.SSH.allowLocalServerDownload": true,
+    "remote.SSH.localServerDownload": "always",
+    "remote.downloadExtensionsLocally": true,
+    "remote.SSH.showLoginTerminal": true,
+    "remote.SSH.useLocalServer": false
+}
