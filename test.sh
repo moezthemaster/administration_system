cb5dfe25ce91406c17dc201f99d3de2f4c9476cc
moezthemaster <moez.selmi1986@gmail.com>
2026-06-13 12:15:16 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@ echo "Run test script!"
 ls -ltra
 pwd
 whoami
+https://www.amazon.fr/dp/B0FNRG39GZ?tag=lechoixgagnancwwc-222217779-21&linkCode=osi&th=1&psc=1&language=fr_FR&ascsubtag=fr_cid2336130698338-8407752
